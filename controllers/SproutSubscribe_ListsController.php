595dece7585b64792b6387d0b51674e892ad506a
vangfc123 <fucheevang@gmail.com>
2015-02-14 01:36:43 -0500

    Fixed spacing, clean up code, and moved code from controller/variable to service.

--- controllers/SproutSubscribe_ListsController.php
+++ controllers/SproutSubscribe_ListsController.php
@@ -1,108 +1,94 @@
 <?php
 namespace Craft;
 
-/**
- * Lists controller
- *
- */
 class SproutSubscribe_ListsController extends BaseController
 {
+    public function actionSubscribe()
+    { 
+        $userId = craft()->userSession->id;
+        $elementId = craft()->request->getPost('elementId');
 
-  public function actionSubscribe()
-  { 
-    $userId = craft()->userSession->id;
-    $elementId = craft()->request->getPost('elementId');
+        if (!$userId OR !$elementId)
+        { 
+            return false;
+        } 
 
-    if (!$userId OR !$elementId)
-    {
-      return false;
+        if (craft()->sproutSubscribe->subscribe($userId, $elementId))
+        {
+            if (craft()->request->isAjaxRequest())
+            {
+                $this->returnJson(array(
+                  'success' => 'success',
+                ));
+            }
+            else
+            {
+                $this->redirectToPostedUrl();
+            }
+        }
+        else
+        {
+            $errors = $record->getErrors();
+
+            if (craft()->request->isAjaxRequest())
+            {
+                $this->returnJson(array(
+                  'errors' => $errors,
+                ));
+            }
+            else
+            {
+                craft()->urlManager->setRouteVariables(array(
+                  'errors' => $errors
+                ));
+
+                $this->redirectToPostedUrl();
+            }
+        }
     }
 
-    $record = new SproutSubscribe_SubscriptionRecord;
-    $record->userId = $userId;
-    $record->elementId = $elementId;
-
-    if ($record->save())
+    public function actionUnsubscribe()
     {
-      if (craft()->request->isAjaxRequest())
-      {
-        $this->returnJson(array(
-          'success' => 'success',
-        ));
-      }
-      else
-      {
-        $this->redirectToPostedUrl();
-      }
-    }
-    else
-    {
-      $errors = $record->getErrors();
+        $userId = craft()->userSession->id;
+        $elementId = craft()->request->getPost('elementId');
 
-      if (craft()->request->isAjaxRequest())
-      {
-        $this->returnJson(array(
-          'errors' => $errors,
-        ));
-      }
-      else
-      {
-        craft()->urlManager->setRouteVariables(array(
-          'errors' => $errors
-        ));
+        if (!$userId OR !$elementId)
+        {
+            return false;
+        }
 
-        $this->redirectToPostedUrl();
-      }
-    }
-  }
+        if (craft()->sproutSubscribe->unsubscribe($userId, $elementId))
+        {       
+            if (craft()->request->isAjaxRequest())
+            {
+                $this->returnJson(array(
+                  'success' => 'success',
+                ));
+            }
+            else
+            {
+                $this->redirectToPostedUrl();
+            }
+        }
+        else
+        {
+            if (craft()->request->isAjaxRequest())
+            {
+                $this->returnJson(array(
+                  'response' => 'fail', 
+                ));
+            }
+            else
+            {
+                craft()->urlManager->setRouteVariables(array(
+                  'response' => 'fail',
+                ));
 
-  public function actionUnsubscribe()
-  {
-    $userId = craft()->userSession->id;
-    $elementId = craft()->request->getPost('elementId');
-
-    if (!$userId OR !$elementId)
-    {
-      return false;
-    }
-
-    $result = craft()->db->createCommand()
-               ->delete('sproutsubscribe_subscriptions', array(
-                  'userId' => $userId,
-                  'elementId' => $elementId
-               ));
-
-    if ($result)
-    {
-      if (craft()->request->isAjaxRequest())
-      {
-        $this->returnJson(array(
-          'success' => 'success',
-        ));
-      }
-      else
-      {
-        $this->redirectToPostedUrl();
-      }
-    }
-    else
-    {
-      if (craft()->request->isAjaxRequest())
-      {
-        $this->returnJson(array(
-          'response' => 'fail',
-        ));
-      }
-      else
-      {
-        craft()->urlManager->setRouteVariables(array(
-          'response' => 'fail',
-        ));
-
-        $this->redirectToPostedUrl();
-      }
-    }
+                $this->redirectToPostedUrl();
+            }
+        }       
 
     $this->redirectToPostedUrl();
-  }
+
+    }
 }
--- variables/SproutSubscribeVariable.php
+++ variables/SproutSubscribeVariable.php
@@ -3,7 +3,6 @@ namespace Craft;
 
 class SproutSubscribeVariable
 {
-
   public function getName()
   {
     $plugin = craft()->plugins->getPlugin('sproutsubscribe');
@@ -18,67 +17,13 @@ class SproutSubscribeVariable
     return $plugin->getVersion();
   }
 
-  // @TODO - move logic to service layer
-  public function isSubscribed($userId = null, $elementId = null)
+  public function isSubscribed($userId, $elementId)
   {
-    if (!$userId or !$elementId)
-    {
-      return false;
-    }
-
-    $query = craft()->db->createCommand()
-            ->select('userId, elementId')
-            ->from('sproutsubscribe_subscriptions')
-            ->where(array(
-              'AND', 
-              'userId = :userId', 
-              'elementId = :elementId'
-            ), array(
-              ':userId' => $userId, 
-              ':elementId' => $elementId
-            ))->queryRow();
-    
-    return (is_array($query)) ? true : false;
+    return craft()->sproutSubscribe->isSubscribed($userId, $elementId);
   }
 
-  // @TODO - move logic to service layer
-  // @TODO - should this be abstracted to any other use cases?
   public function subscriptionIds($userId = null, $elementType = 'Entry', $criteria = array())
   {
-    $userId = craft()->userSession->id;
-    
-    if (!$userId)
-    {
-      return false;
-    }
-
-    // join the sproutsubscribe_subscriptions and elements table to make sure we're only
-    // getting back the IDs of the Elements that match our type.
-
-    $results = craft()->db->createCommand()
-            ->select('elementId')
-            ->from('sproutsubscribe_subscriptions')
-            ->where('userId = :userId', array(
-              ':userId' => $userId
-            ))->queryAll();
-
-    $ids = "";
-
-    foreach ($results as $key => $value) 
-    {
-      if ($ids == "") 
-      {
-        $ids = $value['elementId'];
-      }
-      else
-      { 
-        $ids .= "," . $value['elementId'];
-      }
-    }
-
-    return $ids;
-
+    return craft()->sproutSubscribe->subscriptionIds($userId, $elementType, $criteria);
   }
-
-  
 }

18d56f8642995cfcc8fc8655703524c36a6275a4
Ben Parizek <ben@barrelstrengthdesign.com>
2018-03-03 12:33:31 -0500

    Adds initial type column migration

---
+++ src/migrations/m180303_000000_update_type_to_class.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace barrelstrength\sproutlists\migrations;
+
+use barrelstrength\sproutlists\integrations\sproutlists\SubscriberListType;
+use craft\db\Migration;
+
+/**
+ * Class m180303_000000_update_type_to_class
+ */
+class m180303_000000_update_type_to_class extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        // Link to Url
+        $this->update('{{%sproutlists_lists}}', [
+            'type' => SubscriberListType::class
+        ], [
+            'type' => 'subscriber'
+        ], [], false);
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m180303_000000_update_type_to_class cannot be reverted.\n";
+        return false;
+    }
+}

c149d59adbdf87c787fa404bfa44670395e3978d
Ben Parizek <ben@barrelstrengthdesign.com>
2019-07-19 04:18:18 +0100

    Adds example Segment Report

---
+++ src/integrations/sproutreports/CustomListQuery.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace barrelstrength\sproutlists\integrations\sproutreports\datasources;
+
+use barrelstrength\sproutbasereports\base\DataSource;
+use barrelstrength\sproutbasereports\elements\Report;
+use Craft;
+use Exception;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+/**
+ * Class CustomListQuery
+ *
+ * @package Craft
+ *
+ * @property string $name
+ */
+class CustomListQuery extends DataSource
+{
+    public $whichColumnIsTheEmailColumn = 'userEmail';
+
+    public function getDisplayLocations() {
+
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function displayName(): string
+    {
+        return Craft::t('sprout-reports', 'Custom List Query');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getDescription(): string
+    {
+        return Craft::t('sprout-reports', 'Create a list using a custom database query');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function isAllowHtmlEditable(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getResults(Report $report, array $settings = []): array
+    {
+        $query = $report->getSetting('query');
+
+        $result = [];
+
+        try {
+            $result = Craft::$app->getDb()->createCommand($query)->queryAll();
+        } catch (Exception $e) {
+            $report->setResultsError($e->getMessage());
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param array $settings
+     *
+     * @return string|null
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function getSettingsHtml(array $settings = [])
+    {
+        $settingsErrors = $this->report->getErrors('settings');
+        $settingsErrors = array_shift($settingsErrors);
+
+        return Craft::$app->getView()->renderTemplate('sprout-base-reports/_components/datasources/CustomQuery/settings', [
+            'settings' => count($settings) ? $settings : $this->report->getSettings(),
+            'errors' => $settingsErrors
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function validateSettings(array $settings = [], array &$errors = []): bool
+    {
+        if (empty($settings['query'])) {
+            $errors['query'][] = Craft::t('sprout-reports', 'Query cannot be blank.');
+
+            return false;
+        }
+
+        return true;
+    }
+}

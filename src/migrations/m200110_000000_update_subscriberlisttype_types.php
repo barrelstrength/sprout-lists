f053ab31b20304f19d4ebdd18d1930b3c6eb3149
Ben Parizek <ben.parizek@gmail.com>
2020-01-10 09:00:57 -0400

    Adds class migration

---
+++ src/migrations/m200110_000000_update_subscriberlisttype_types.php
@@ -0,0 +1,38 @@
+<?php /** @noinspection ClassConstantCanBeUsedInspection */
+
+namespace barrelstrength\sproutlists\migrations;
+
+use craft\db\Migration;
+
+class m200110_000000_update_subscriberlisttype_types extends Migration
+{
+    /**
+     * @return bool
+     */
+    public function safeUp(): bool
+    {
+        $mailingClasses = [
+            0 => [
+                'oldType' => 'barrelstrength\sproutbaselists\listtypes\MailingList',
+                'newType' => 'barrelstrength\sproutlists\listtypes\SubscriberList'
+            ]
+        ];
+
+        foreach ($mailingClasses as $mailingClass) {
+            $this->update('{{%sproutlists_lists}}', [
+                'type' => $mailingClass['newType']
+            ], ['type' => $mailingClass['oldType']], [], false);
+        }
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown(): bool
+    {
+        echo "m200110_000000_update_subscriberlisttype_types cannot be reverted.\n";
+        return false;
+    }
+}
